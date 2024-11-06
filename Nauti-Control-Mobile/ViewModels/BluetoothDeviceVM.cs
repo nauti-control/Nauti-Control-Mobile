ce4e0285dcdc3564569e0e499700d4ae30a1cbf7
philipblack78@gmail.com <philipblack78@gmail.com>
2024-11-06 20:54:48 +0000

    Starting Bluetooth Scanner

---
+++ Nauti-Control-Mobile/ViewModels/BluetoothDeviceVM.cs
@@ -0,0 +1,19 @@
+﻿using Plugin.BLE.Abstractions.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nauti_Control_Mobile.ViewModels
+{
+    public class BluetoothDeviceVM
+    {
+        private IDevice _bluetoothDevice;
+
+        public BluetoothDeviceVM(IDevice bluetoothDevice)
+        {
+            _bluetoothDevice = bluetoothDevice;
+        }
+    }
+}
---
+++ Nauti-Control-Mobile/ViewModels/BluetoothManagerVM.cs
@@ -0,0 +1,40 @@
+﻿using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nauti_Control_Mobile.ViewModels
+{
+    public class BluetoothManagerVM
+    {
+        private IBluetoothLE _ble;
+        private IAdapter _adapter;
+
+        public ObservableCollection<BluetoothDeviceVM> DeviceVMs { get; private set; }
+
+        public BluetoothManagerVM()
+        {
+            _ble = CrossBluetoothLE.Current;
+            _adapter = CrossBluetoothLE.Current.Adapter;
+            _adapter.DeviceDiscovered += DeviceDiscovered;
+            _adapter.ScanMode = ScanMode.Balanced;
+        }
+
+        private void DeviceDiscovered(object? sender, Plugin.BLE.Abstractions.EventArgs.DeviceEventArgs e)
+        {
+
+            BluetoothDeviceVM vm = new BluetoothDeviceVM(e.Device);
+            DeviceVMs.Add(vm);
+
+        }
+
+        public async Task StartScanning()
+        {
+            await _adapter.StartScanningForDevicesAsync();
+        }
+    }
+}
