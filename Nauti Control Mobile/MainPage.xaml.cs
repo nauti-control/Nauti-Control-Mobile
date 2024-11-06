5ecdd8c0d0f3f057c2b038c56f3f26c3e18b07b4
philipblack78@gmail.com <philipblack78@gmail.com>
2024-11-06 18:58:59 +0000

    Initial Project Creation.

---
+++ Nauti Control Mobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Nauti_Control_Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
