1ea0f71f774ebf5091b99f3d7aee490d78725470
philipblack78@gmail.com <philipblack78@gmail.com>
2024-11-06 19:39:57 +0000

    Fixed Startup with stylign for bootstrap

---
+++ Nauti-Control-Mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Nauti_Control_Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
